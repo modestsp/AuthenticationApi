944a9793314d5955293525a4e9032656522fb377
modestsp <csperichon@gmail.com>
2023-07-26 20:23:51 -0300

    Created Seed manager for admin user

---
+++ AuthenticationApi/Database/SeedManager.cs
@@ -0,0 +1,39 @@
+using AuthenticationApi.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthenticationApi.Database;
+
+public static class SeedManager
+{
+    public static async Task Seed(IServiceProvider services)
+    {
+        await SeedRoles(services);
+
+        await SeedAdminUser(services);
+    }
+
+    private static async Task SeedRoles(IServiceProvider services)
+    {
+        var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+
+        await roleManager.CreateAsync(new IdentityRole(Role.Admin));
+        await roleManager.CreateAsync(new IdentityRole(Role.User));
+    }
+
+    private static async Task SeedAdminUser(IServiceProvider services)
+    {
+        var context = services.GetRequiredService<AppDbContext>();
+        var userManager = services.GetRequiredService<UserManager<User>>();
+        var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+
+        var adminUser = await context.Users.FirstOrDefaultAsync(user => user.UserName == "AuthenticationAdmin");
+
+        if (adminUser == null)
+        {
+            adminUser = new User { UserName = "AuthenticationAdmin", Email = "admin@mail.com" };
+            await userManager.CreateAsync(adminUser, "Secret-123");
+            await userManager.AddToRoleAsync(adminUser, Role.Admin);
+        }
+    }
+}
